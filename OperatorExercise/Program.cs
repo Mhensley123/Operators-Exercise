bc31de10f400ab8d02be961f1f6f5ca3a993ad53
Maria Hensley <mscelie34@zoho.com>
2022-11-05 21:07:19 -0500

    finished operators

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+
+            var i = "3";
+            var j = "4";
+            var k = "++i * j++";
+            Console.WriteLine($"k = {i} * {j}");
+
+            //Allow user to input the radius
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            //Calculate area of the circle
+            var areaOfCircle = CalculateArea(radius);
+
+            Console.WriteLine($"The area of a circle with radius of {radius} is {areaOfCircle}");
+
         }
+
+        public static double CalculateArea(double radius)
+        {
+            return Math.PI * (radius * radius);
+        }
+        
+        
+
+        
+      
+
+
+
     }
 }
